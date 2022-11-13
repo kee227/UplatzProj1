1f8fa49fdbe985012553fc266f1024f647ed8f14
kee227 <reddykeerthi.maddi@gmail.com>
2022-11-13 12:48:26 +0530

    Initial comment

---
+++ src/test/java/com/uplatz/in/UplatzProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplatz.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UplatzProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
